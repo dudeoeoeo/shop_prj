901d75bdfbf1e24eabd0b69a861dcc556502dc55
dudeoeoeo <dudekekek@naver.com>
2021-10-14 13:11:15 +0900

    Item Entity

--- src/main/java/com/shop/entity/Item.java
+++ src/main/java/com/shop/entity/Item.java
@@ -1,4 +1,40 @@
 package com.shop.entity;
 
+import com.shop.constant.ItemSellStatus;
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "item")
+@Getter
+@Setter
+@ToString
 public class Item {
+
+    @Id
+    @Column(name = "item_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 50)
+    private String itemName;
+
+    @Column(name = "price", nullable = false)
+    private int price;
+
+    @Column(nullable = false)
+    private int stockNumber;
+
+    @Lob
+    @Column(nullable = false, length = 500)
+    private String itemDetail;
+
+    @Enumerated(EnumType.STRING)
+    private ItemSellStatus itemSellStatus;
+
+    private LocalDateTime regTime;
+
+    private LocalDateTime updateTime;
 }

f1c0a9abf028b4786384b24cd163c5fe5a36393e
dudeoeoeo <dudekekek@naver.com>
2021-10-17 15:06:17 +0900

    member

--- src/main/java/com/shop/entity/Member.java
+++ src/main/java/com/shop/entity/Member.java
@@ -40,6 +40,7 @@ public class Member {
         member.setAddress(memberFormDto.getAddress());
         String password = passwordEncoder.encode(memberFormDto.getPassword());
         member.setPassword(password);
+        //member.setRole(Role.USER);
         member.setRole(Role.USER);
         return member;
     }

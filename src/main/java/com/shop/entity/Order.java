b7bc390cd4f187ea728c856a426ced80e9dca6ca
dudeoeoeo <dudekekek@naver.com>
2021-10-18 17:56:03 +0900

    Order Entity

---
+++ src/main/java/com/shop/entity/Order.java
@@ -0,0 +1,38 @@
+package com.shop.entity;
+
+import com.shop.constant.OrderStatus;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "orders")
+@Getter
+@Setter
+public class Order extends BaseEntity{
+
+    @Id
+    @GeneratedValue
+    @Column(name = "order_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    // orphanRemoval 고아 객체 제거하기
+    // 고아 객체 제거 기능은 참조하는 곳이 하나일 때만 사용해야 한다.
+    // 다른 곳에서도 참조하고 있는 엔티티인데 삭제하면 문제가 생길 수 있다.
+    // @OndToOne, @OneToMany 어노테이션에서 옵션으로 사용하면 된다.
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
+    private List<OrderItem> orderItems = new ArrayList<>();
+
+    private LocalDateTime orderDate;
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus orderStatus;
+}
